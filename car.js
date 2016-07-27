788f270cdc8450e498faa4fe941eba7a13b688fe
Jamie <lanae_j@operamail.com>
2016-07-27 09:58:03 -0600

    finished warmup

--- car.js
+++ car.js
@@ -1,9 +1,33 @@
-
+'use strict';
 
 var Car = function (mpg) {
+  this.distance=0;
+  this.odom=0;
+  this.mpg=mpg;
+  this.tripstaken=[];
+  if(this.gallons===undefined){
+    this.gallons=0;
+  }
+
+  var gal;
+gal+=this.gallons;
+return gal;
 };
 
 Car.prototype.fill = function (gallons) {
+  this.gallons+=gallons;
 };
-
+Car.prototype.drive=function(distance){
+  this.distance=distance;
+  this.tripstaken.push(this.distance + " miles");
+  this.gallons=this.gallons-(this.distance/this.mpg);
+  return this.gallons;
+};
+Car.prototype.odometer=function(){
+  this.odom=this.odom+this.distance;
+  return this.odom;
+};
+Car.prototype.trips=function(){
+  return this.tripstaken;
+};
 module.exports = Car;
